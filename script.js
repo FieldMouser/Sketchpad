fea8b6d5293f68647f034c124ae293fb0220eea0
FieldMouser <vlad01.com98@gmail.com>
2024-07-06 01:31:04 +0700

    Changed style, added grid size changing, updated color selection sliders

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 const container = document.querySelector(".container");
 const pickedColor = document.querySelector(".pickedColor");
 let red = 0, green = 0, blue = 0;
+
+let size = document.querySelector(".squareSize").value;
+if (size > 100) {
+    size = 100;
+    document.querySelector(".squareSize").value = size;
+}
+else if (size < 2) {
+    size = 2;
+    document.querySelector(".squareSize").value = size;
+}
+
 pickedColor.style.backgroundColor = `rgb(${red},${green},${blue})`;
+document.querySelector(".red").value = red;
+document.querySelector(".blue").value = blue;
+document.querySelector(".green").value = green;
 
 function randomColor(){
     red = Math.round(Math.random() * 255);
     blue = Math.round(Math.random() * 255);
     green = Math.round(Math.random() * 255);
     pickedColor.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    document.querySelector(".red").value = red;
+    document.querySelector(".blue").value = blue;
+    document.querySelector(".green").value = green;
 }
 
 function redColor(value) {
@@ -25,10 +42,19 @@ function blueColor(value) {
     blue = value;   
 }
 
+function changeSize() {
+    location.reload();
+    size = document.querySelector(".squareSize").value;
+    
+    
+}
 
-for (let i = 0; i < 255; i++) {
+
+for (let i = 0; i < size * size; i++) {
     const div = document.createElement("div"); // Создание нового элемента div
     div.classList.add("pixel"); // Добавление класса pixel к новому элементу
+    div.style.width = 512 / size - 2 + "px";
+    div.style.height = 512 / size - 2 + "px";
     container.appendChild(div); // Добавление нового элемента в контейнер
 
     div.onmousedown = () => {
